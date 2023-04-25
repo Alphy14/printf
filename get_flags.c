0791643f0bc15dc1957d54194420c796dc3daf06
Alfred <alfredoseibossman99@gmail.com>
2023-04-25 04:47:33 -0700

    get_flags.c

---
+++ get_flags.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * get_flags - Calcs active flags
+ * @format: Formatted string
+ * @i: take a parameter
+ * Return: Flags
+ */
+int get_flags(const char *format, int *i)
+{
+	int a, curr_i;
+	int flags = 0;
+
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	{
+		for (a = 0; FLAGS_ARR[a] != '\0'; a++)
+		{
+			if (format[curr_i] == FLAGS_ARR[a])
+			{
+				flags |= FLAGS_ARR[j];
+				break;
+			}
+		}
+		if (FLAGS_ARR[a] == 0)
+			break;
+	}
+	*i = curr_i - 1;
+	return (flags);
+}
+
